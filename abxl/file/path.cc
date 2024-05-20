0549df9f497fe3281ee4fbc5473841f25d900fbe
David Z. Chen <david@davidzchen.com>
2024-05-20 01:34:29 -0700

    Use ortools path library instead.

--- abxl/file/path.cc
+++ abxl/file/path.cc
@@ -1,10 +1,9 @@
-// Copyright 2019 The MediaPipe Authors.
-//
+// Copyright 2010-2024 Google LLC
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,10 +13,10 @@
 
 #include "abxl/file/path.h"
 
+#include <cstring>
 #include <string>
 
 #include "absl/strings/str_cat.h"
-#include "absl/strings/string_view.h"
 
 namespace abxl {
 
@@ -50,7 +49,7 @@ std::string JoinPathImpl(bool honor_abs,
     // This size calculation is worst-case: it assumes one extra "/" for every
     // path other than the first.
     size_t total_size = paths.size() - 1;
-    for (const absl::string_view& path : paths)
+    for (const absl::string_view path : paths)
       total_size += path.size();
     result.resize(total_size);
 
@@ -87,7 +86,7 @@ std::pair<absl::string_view, absl::string_view> SplitBasename(
     absl::string_view path) {
   path = Basename(path);
 
-  absl::string_view::size_type pos = path.find_last_of('.');
+  size_t pos = path.find_last_of('.');
   if (pos == absl::string_view::npos)
     return std::make_pair(path, absl::ClippedSubstr(path, path.size(), 0));
   return std::make_pair(path.substr(0, pos),
@@ -95,6 +94,19 @@ std::pair<absl::string_view, absl::string_view> SplitBasename(
 }
 
 }  // namespace internal
+
+bool IsAbsolutePath(absl::string_view path) {
+  return !path.empty() && path[0] == '/';
+}
+
+std::string AddSlash(absl::string_view path) {
+  size_t length = path.size();
+  if (length && path[length - 1] != '/') {
+    return absl::StrCat(path, "/");
+  } else {
+    return std::string(path);
+  }
+}
 
 absl::string_view Dirname(absl::string_view path) {
   return SplitPath(path).first;
@@ -106,7 +118,7 @@ absl::string_view Basename(absl::string_view path) {
 
 std::pair<absl::string_view, absl::string_view> SplitPath(
     absl::string_view path) {
-  absl::string_view::size_type pos = path.find_last_of('/');
+  size_t pos = path.find_last_of('/');
 
   // Handle the case with no '/' in 'path'.
   if (pos == absl::string_view::npos)
@@ -120,8 +132,112 @@ std::pair<absl::string_view, absl::string_view> SplitPath(
                         absl::ClippedSubstr(path, pos + 1));
 }
 
+absl::string_view Stem(absl::string_view path) {
+  return internal::SplitBasename(path).first;
+}
+
 absl::string_view Extension(absl::string_view path) {
   return internal::SplitBasename(path).second;
+}
+
+std::string CleanPath(const absl::string_view unclean_path) {
+  std::string path = std::string(unclean_path);
+  const char* src = path.c_str();
+  std::string::iterator dst = path.begin();
+
+  // Check for absolute path and determine initial backtrack limit.
+  const bool is_absolute_path = *src == '/';
+  if (is_absolute_path) {
+    *dst++ = *src++;
+    while (*src == '/')
+      ++src;
+  }
+  std::string::const_iterator backtrack_limit = dst;
+
+  // Process all parts
+  while (*src) {
+    bool parsed = false;
+
+    if (src[0] == '.') {
+      //  1dot ".<whateverisnext>", check for END or SEP.
+      if (src[1] == '/' || !src[1]) {
+        if (*++src) {
+          ++src;
+        }
+        parsed = true;
+      } else if (src[1] == '.' && (src[2] == '/' || !src[2])) {
+        // 2dot END or SEP (".." | "../<whateverisnext>").
+        src += 2;
+        if (dst != backtrack_limit) {
+          // We can backtrack the previous part
+          for (--dst; dst != backtrack_limit && dst[-1] != '/'; --dst) {
+            // Empty.
+          }
+        } else if (!is_absolute_path) {
+          // Failed to backtrack and we can't skip it either. Rewind and copy.
+          src -= 2;
+          *dst++ = *src++;
+          *dst++ = *src++;
+          if (*src) {
+            *dst++ = *src;
+          }
+          // We can never backtrack over a copied "../" part so set new limit.
+          backtrack_limit = dst;
+        }
+        if (*src) {
+          ++src;
+        }
+        parsed = true;
+      }
+    }
+
+    // If not parsed, copy entire part until the next SEP or EOS.
+    if (!parsed) {
+      while (*src && *src != '/') {
+        *dst++ = *src++;
+      }
+      if (*src) {
+        *dst++ = *src++;
+      }
+    }
+
+    // Skip consecutive SEP occurrences
+    while (*src == '/') {
+      ++src;
+    }
+  }
+
+  // Calculate and check the length of the cleaned path.
+  int path_length = dst - path.begin();
+  if (path_length != 0) {
+    // Remove trailing '/' except if it is root path ("/" ==> path_length := 1)
+    if (path_length > 1 && path[path_length - 1] == '/') {
+      --path_length;
+    }
+    path.resize(path_length);
+  } else {
+    // The cleaned path is empty; assign "." as per the spec.
+    path.assign(1, '.');
+  }
+  return path;
+}
+
+std::string CollapseSlashes(absl::string_view path) {
+  std::string ret;
+  ret.reserve(path.size());
+  bool prev_was_slash = false;
+  for (char c : path) {
+    if (c == '/') {
+      if (prev_was_slash) {
+        continue;
+      }
+      prev_was_slash = true;
+    } else {
+      prev_was_slash = false;
+    }
+    ret.push_back(c);
+  }
+  return ret;
 }
 
 }  // namespace abxl
--- abxl/file/path.h
+++ abxl/file/path.h
@@ -1,10 +1,9 @@
-// Copyright 2019 The MediaPipe Authors.
-//
+// Copyright 2010-2024 Google LLC
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,22 +11,46 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#ifndef ABXL_PATH_H_
-#define ABXL_PATH_H_
+#ifndef ABXL_FILE_PATH_H_
+#define ABXL_FILE_PATH_H_
 
 #include <initializer_list>
 #include <string>
+#include <utility>
 
 #include "absl/strings/string_view.h"
 
 // A set of file pathname manipulation routines.
-namespace abxl {
-namespace internal {
+// Calls to each of the following functions assume their input is
+// well-formed (for some currently nebulous definition of the word).
+// You can get a well-formed path by using abxl::CleanPath(), and
+// future versions of this API may require this.
+//
+// This collection is largely modelled on Python's os.path module.
 
+// Filenames are restricted to ASCII characters in most filesystems at
+// Google.  There are additional restrictions and quirks for some
+// filesystems.  For example, see file/colossus/base/cfs_path.h for
+// filename character set restrictions for Colossus.
+//
+// One additional filename quirk: for legacy reasons, filenames of
+// the forms:
+//  localhost:pathname
+//  hostname:pathname
+//  hostname.domainname:pathname
+// will all route to //file/localfile and use only the pathname part, so
+// long as hostname/hostname.domainname refer to the current host.  The
+// primary implication of this behavior is that filenames that do not
+// begin with a leading '/' and that have a ':' before any '/' may be
+// interpreted differently depending on the hostname where they are
+// evaluated.
+
+namespace abxl {
+
+namespace internal {
 // Not part of the public API.
 std::string JoinPathImpl(bool honor_abs,
                          std::initializer_list<absl::string_view> paths);
-
 }  // namespace internal
 
 // Join multiple paths together.
@@ -38,20 +61,22 @@ std::string JoinPathImpl(bool honor_abs,
 //
 //  Arguments                  | JoinPath            | JoinPathRespectAbsolute
 //  ---------------------------+---------------------+-----------------------
-//  '/foo', 'bar'              | /foo/bar            | /foo/bar
+//  'foo',   'bar'             | foo/bar             | foo/bar
+//  '/foo',  'bar'             | /foo/bar            | /foo/bar
 //  '/foo/', 'bar'             | /foo/bar            | /foo/bar
-//  '/foo', '/bar'             | /foo/bar            | /bar
-//  '/foo', '/bar', '/baz'     | /foo/bar/baz        | /baz
+//  '/foo',  '/bar'            | /foo/bar            | /bar
+//  '/foo/', '/bar'            | /foo/bar            | /bar
+//  '/foo',  '/bar', '/baz'    | /foo/bar/baz        | /baz
 //
-// All paths will be treated as relative paths, regardless of whether or not
-// they start with a leading '/'.  That is, all paths will be concatenated
-// together, with the appropriate path separator inserted in between.
+// The first path may be relative or absolute.  All subsequent paths will be
+// treated as relative paths, regardless of whether or not they start with a
+// leading '/'.  That is, all paths will be concatenated together, with the
+// appropriate path separator inserted in between.
 // Arguments must be convertible to absl::string_view.
 //
 // Usage:
-// std::string path = file::JoinPath("/cns", dirname, filename);
-// std::string path = file::JoinPath("./",
-//                                   filename);
+// string path = abxl::JoinPath("/cns", dirname, filename);
+// string path = abxl::JoinPath(FLAGS_test_srcdir, filename);
 //
 // 0, 1, 2-path specializations exist to optimize common cases.
 inline std::string JoinPath() { return std::string(); }
@@ -62,8 +87,30 @@ std::string JoinPath(absl::string_view path1, absl::string_view path2);
 template <typename... T>
 inline std::string JoinPath(absl::string_view path1, absl::string_view path2,
                             absl::string_view path3, const T&... args) {
-  return internal::JoinPathImpl(false, {path1, path2, path3, args...});
+  return internal::JoinPathImpl(/*honor_abs=*/false,
+                                {path1, path2, path3, args...});
 }
+
+// Join multiple paths together, respecting intermediate absolute paths.
+// All paths will be joined together, but if any of the paths is absolute
+// (as defined by IsAbsolutePath()), all prior path segments will be ignored.
+// This is the behavior of the old abxl::JoinPath().
+// Arguments must be convertible to absl::string_view.
+//
+// Usage:
+// string path = abxl::JoinPathRespectAbsolute("/cns", dirname, filename);
+// string path = abxl::JoinPathRespectAbsolute(FLAGS_test_srcdir, filename);
+template <typename... T>
+inline std::string JoinPathRespectAbsolute(const T&... args) {
+  return internal::JoinPathImpl(/*honor_abs=*/true, {args...});
+}
+
+// Return true if path is absolute.
+bool IsAbsolutePath(absl::string_view path);
+
+// If path is non-empty and doesn't already end with a slash, append one
+// to the end.
+std::string AddSlash(absl::string_view path);
 
 // Returns the part of the path before the final "/", EXCEPT:
 // * If there is a single leading "/" in the path, the result will be the
@@ -86,10 +133,41 @@ std::pair<absl::string_view, absl::string_view> SplitPath(
 // empty string.
 absl::string_view Basename(absl::string_view path);
 
+// Returns the part of the basename of path prior to the final ".".  If
+// there is no "." in the basename, this is equivalent to abxl::Basename(path).
+absl::string_view Stem(absl::string_view path);
+
 // Returns the part of the basename of path after the final ".".  If
 // there is no "." in the basename, the result is empty.
 absl::string_view Extension(absl::string_view path);
 
+// ********************************************************
+// Path cleaning utilities.
+//
+// NOTE: FileFactory implementations should probably clean paths they
+// receive as needed.  They should only clean the pieces of path they
+// understand (in the case of wrapping-functionality like
+// /readaheadfile or /encryptedfile).  It cannot universally be done
+// cleanly with common logic.
+//
+// Common sorts of surprises along those lines:
+//  * file/zipfile allows '/' and '.' in file *names* (and has no directory
+//    structure).
+//  * Colossus probably shouldn't collapse "ttl=8/../" into "/"
+
+// Collapse duplicate "/"s, resolve ".." and "." path elements, remove
+// trailing "/".
+//
+// NOTE: This respects relative vs. absolute paths, but does not
+// invoke any system calls (getcwd(2)) in order to resolve relative
+// paths wrt actual working directory.  That is, this is purely a
+// string manipulation, completely independent of process state.
+std::string CleanPath(absl::string_view path);
+
+// Similar to CleanPath, but only collapses duplicate "/"s.  (Some
+// filesystems allow "." and ".." in filenames.)
+std::string CollapseSlashes(absl::string_view path);
+
 }  // namespace abxl
 
-#endif  // ABXL_PATH_H_
+#endif  // ABXL_FILE_PATH_H_
