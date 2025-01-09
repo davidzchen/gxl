b2c0e89c042382d8772b547c05d9b87077869dbe
David Z. Chen <david@davidzchen.com>
2025-01-09 17:09:49 -0300

    Rename to gxl and fix all build errors.

---
+++ gxl/base/types.h
@@ -0,0 +1,12 @@
+// -----------------------------------------------------------------------------
+// File: base.h
+// -----------------------------------------------------------------------------
+//
+// This header contains low-level common definitions.
+
+#ifndef GXL_BASE_TYPES_H_
+#define GXL_BASE_TYPES_H_
+
+typedef signed int char32;
+
+#endif  // GXL_BASE_TYPES_H_
